f13adf5522302a7aa2401bdfe018947a9201709c
[Le Van Phuong] <[phuongpew@gmail.com]>
2023-07-16 10:52:52 +0700

    feat:be-create-api-delete-category Hoàn thành phần create api delete category Chỉnh lại phần getAll ref: DTNF-20

--- BE/routers/category.js
+++ BE/routers/category.js
@@ -4,4 +4,5 @@ import categoryController from "../modules/Category/Controller/index.js";
 const router = express.Router()
 router.get('/', categoryController.getAllCategorys)
 router.get('/:id', categoryController.getOneCategorys)
+router.delete('/:id', categoryController.deleteCategorys)
 export default router
