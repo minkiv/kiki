45141b388b0d2ab946dd7e0c7e56d4bc1da2d8ba
kieutruc <kieubuithanhtruc2307@gmail.com>
2023-07-16 14:12:56 +0700

    feat:DTNF-21-be-create-api-post-category refs:DTNF-21 tôi đã làm xong phần create-api-post-category

--- BE/modules/Category/Service/Category.js
+++ BE/modules/Category/Service/Category.js
@@ -12,3 +12,11 @@ export const deleteCategory = async(req,res)=>{
     const remove = await Category.findByIdAndDelete(req.params.id)
     return remove
 }
+export const addCategory = async (req) => {
+    const fileImage = req.file
+    const categorys = await Category.create({
+        ...req.body,
+        image: fileImage.path
+    })
+    return categorys
+}
--- BE/routers/category.js
+++ BE/routers/category.js
@@ -1,8 +1,10 @@
 import express from "express";
 import categoryController from "../modules/Category/Controller/index.js";
-
+import upload from '../config/configImage.js'
 const router = express.Router()
 router.get('/', categoryController.getAllCategorys)
 router.get('/:id', categoryController.getOneCategorys)
+router.use(upload.single('file'))
 router.delete('/:id', categoryController.deleteCategorys)
+router.post('/add', categoryController.addCategorys)
 export default router
