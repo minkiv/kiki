0cf008060381810f0fbcde07d84c80402a1c0742
hiepph <phanhuyhiep2k3@gmail.com>
2023-07-16 09:13:21 +0700

    feat:be-create-api-get-one-category Tôi đã làm xong chức năng getOnecategory refs:DTNF-19

--- BE/routers/category.js
+++ BE/routers/category.js
@@ -3,4 +3,5 @@ import categoryController from "../modules/Category/Controller/index.js";
 
 const router = express.Router()
 router.get('/', categoryController.getAllCategorys)
+router.get('/:id', categoryController.getOneCategorys)
 export default router
