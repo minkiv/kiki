602bfafa0e812ebc712f6f2986fc8d6192bff14b
phuongpew@gmail.com <phuongpew@gmail.com>
2023-07-18 23:09:35 +0700

    feat: be-create-model-api-auth Hoàn thành phần create model api auth ref: DTNF-24

---
+++ BE/modules/Auth/Model/Auth.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose'
+const authSchema= mongoose.Schema({
+    fullname: String,
+    nickname: String,
+    email:{
+        type: String,
+        default:''
+    },
+    birth: {
+        type: Date
+    },
+    phoneNumber:{
+        type:String,
+        default:''
+    },
+    password: {
+        type: String,
+        require: true,
+        min: 8
+    },
+    gender: {
+        type: String,
+        enum: ["male", "female", "orther"],
+        default: "male"
+    },
+    nationality: {
+        type: String,
+        default: "",
+        require: true
+    },
+    role: {
+        type: String,
+        enum: ["USER", "USER_STORE", "ADMIN"],
+        default: "USER"
+    }
+}, {
+    timestamps: true
+})
+export default mongoose.model("Auth", authSchema)
