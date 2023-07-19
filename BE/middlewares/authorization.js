580e0298745286ac1e7f2842b4a05db9e9dd7105
kieutruc <kieubuithanhtruc2307@gmail.com>
2023-07-20 00:03:03 +0700

    feact: DTNF-27-be-create-middlewares t đã làm xong phần check middlewares refs: DTNF-27

---
+++ BE/middlewares/authorization.js
@@ -0,0 +1,41 @@
+import Auth from "../modules/Auth/Model/Auth.js";
+import status from "http-status";
+import jwt from "jsonwebtoken"
+const veryfiletoken = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization
+        const accessToken = token.split(" ")[1]
+        if (!token) {
+            return res.status(status.BAD_REQUEST).json('Bạn chưa đăng nhập')
+        }
+        const decoder = await jwt.verify(accessToken, process.env.SECRET_KEY)
+        if (!decoder) {
+            return res.status(status.BAD_REQUEST).json('lỗi token')
+        }
+        const user = await Auth.findOne({ _id: decoder.id })
+        req.User = user
+        next()
+
+    } catch (error) {
+        return res.status(status.INTERNAL_SERVER_ERROR).json('lỗi')
+    }
+}
+const checkAdminAuthorization = (req,res,next) =>{
+    veryfiletoken(req,res,()=>{
+        if (req.User.role == "ADMIN"){
+            next()
+        }else{
+            return res.status(status.UNAUTHORIZED).json('Thất Bại')
+        }
+    })
+}
+const checkUserStoreAuthorization = (req,res,next) =>{
+    veryfiletoken(req,res,()=>{
+        if (req.User.role == "USER_STORE"){
+            next()
+        }else{
+            return res.status(status.UNAUTHORIZED).json('Thất Bại')
+        }
+    })
+}
+export {checkAdminAuthorization,checkUserStoreAuthorization,veryfiletoken}
