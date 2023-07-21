6dee442d3b3adaa4b629e6ced6ce57f7e39281e3
kieutruc <kieubuithanhtruc2307@gmail.com>
2023-07-21 09:33:06 +0700

    feact: DTNF-34-be-create-api-add-carts t đã làm xong phần addCart refs: DTNF-34

--- BE/modules/Cart/Service/Cart.js
+++ BE/modules/Cart/Service/Cart.js
@@ -9,7 +9,12 @@ export const getAllCart= async(req,res)=>{
     const getAll = await Cart.find().populate("carts");
     return getAll
 }
-
+export const addCart = async(req,res)=>{
+    const add = await Cart.create(
+        req.body
+    )
+   return add
+}
 export const updateCart = async(req,res) => {
     const id = req.params.id
     const update = await Cart.updateOne({
