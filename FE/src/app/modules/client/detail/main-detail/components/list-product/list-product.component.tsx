933cdd2214643f6c58f59427961162979bbd4773
phuongpew@gmail.com <phuongpew@gmail.com>
2023-07-31 23:22:49 +0700

    bug: fe-create-componet-ui-product-detail-of-the-client Sửa lại phần create ui detail product ref: DTNF-16

--- FE/src/app/modules/client/detail/main-detail/components/list-product/list-product.component.tsx
+++ FE/src/app/modules/client/detail/main-detail/components/list-product/list-product.component.tsx
@@ -1,36 +1,33 @@
 import React, { FunctionComponent } from 'react'
 import { css } from '@emotion/react'
 import ItemProduct from '~/app/component/parts/itemproduct/itemproduct.component'
+import SwiperList from '~/app/component/stack/swiper-list/swiper-list.component'
+import { SwiperSlide } from 'swiper/react'
 
-interface Listproduct{
+interface Listproduct {
     props?: any
 }
-const Listproduct:FunctionComponent<Listproduct> = () => {
-  return (
-    <div className='bg-slate-50 p-5'>
-    <h2 className='text-3xl pb-5'>Sản phẩm tương tự</h2>
-    <div className='list-product flex'>
-        <div className='px-2'>
-            <ItemProduct/>
+const Listproduct: FunctionComponent<Listproduct> = () => {
+    const arrayImage = [
+        '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'
+    ]
+    return (
+        <div className='bg-slate-50 p-5 bg-white'>
+            <h2 className='text-3xl pb-5'>Sản phẩm tương tự</h2>
+            <div className='flex'>
+                <SwiperList>
+                    {arrayImage?.map((item, index) => (
+                        <SwiperSlide key={index}>
+                            <ItemProduct />
+                        </SwiperSlide>
+                    ))}
+                </SwiperList>
+            </div>
         </div>
-        <div className='px-2'>
-            <ItemProduct/>
-        </div>
-        <div className='px-2'>
-            <ItemProduct/>
-        </div>
-        <div className='px-2'>
-            <ItemProduct/>
-        </div>
-        <div className='px-2'>
-            <ItemProduct/>
-        </div>
-    </div>
-    </div>
-  )
+    )
 }
 
 export default Listproduct
-const cssList = css `
+const cssList = css`
 
 `
