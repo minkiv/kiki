3c8af901f1ae039f5f9716869daa070f58fe27b5
phuongpew@gmail.com <phuongpew@gmail.com>
2023-12-14 20:33:20 +0700

    feat: synchronize data when performing functions Hoàn thành performing functions refs: DTNF-121

--- BE/server.js
+++ BE/server.js
@@ -1,15 +1,11 @@
-import express from "express";
-import cors from "cors";
-import http from "http";
-import { Server } from "socket.io";
-import connectDB from "./config/connectDB.js";
-import dotenv from "dotenv";
-import router from "./routers/router.js";
-
-dotenv.config();
-
-const app = express();
-app.use(cors());
+import express from "express"
+import cors from "cors"
+import connectDB from "./config/connectDB.js"
+import dotenv from "dotenv"
+import router from "./routers/router.js"
+dotenv.config()
+const app = express()
+app.use(cors())
 app.use((req, _, next) => {
     req.headers.origin = req.headers.origin || req.headers.host;
     if (!req.headers.origin.startsWith('http')) {
@@ -19,31 +15,19 @@ app.use((req, _, next) => {
     req.ipv4 = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     next();
 });
-app.use(express.json());
+app.use(express.json())
 
-const server = http.createServer(app);
-const socketIo = new Server(server);
+connectDB()
 
-connectDB();
+// app.use('/api/v1', routes)
+app.use("/api", router)
 
-app.use("/api", router);
-
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT || 8001
 
 app.get('/', (req, res) => {
-    return res.send('Start server success!');
-});
+    return res.send('Start server success!')
+})
 
-socketIo.on("connection", (socket) => {
-    console.log("A user connected");
-    socket.on("message", (data) => {
-        console.log("Received message:", data);
-        socketIo.emit("message", data);
-    });
-    socket.on("disconnect", () => {
-        console.log("A user disconnected");
-    });
-});
-server.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
-});
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+})
--- FE/src/app/api/configHttp.ts
+++ FE/src/app/api/configHttp.ts
@@ -1,41 +1,37 @@
 import axios from 'axios'
-import { io } from "socket.io-client";
-const accessToken = localStorage.getItem("accessToken")
+const accessToken = localStorage.getItem('accessToken')
 export const axiosPrivate = axios.create({
-    baseURL: "http://localhost:8081/api",
-    headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        Authorization: `Bearer ${accessToken}`
-    }
+  baseURL: 'http://localhost:8081/api',
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    Authorization: `Bearer ${accessToken}`
+  }
 })
 
 export const axiosFormData = axios.create({
-    baseURL: "http://localhost:8081/api",
-    headers: {
-        'Content-Type': 'multipart/form-data',
-        'Access-Control-Allow-Origin': '*',
-        Authorization: `Bearer ${accessToken}`
-    }
+  baseURL: 'http://localhost:8081/api',
+  headers: {
+    'Content-Type': 'multipart/form-data',
+    'Access-Control-Allow-Origin': '*',
+    Authorization: `Bearer ${accessToken}`
+  }
 })
 
 axios.interceptors.request.use(
-    function (config) {
-        return config
-    },
-    function (error) {
-        return Promise.reject(error)
-    }
+  function (config) {
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
 )
 
 axios.interceptors.response.use(
-    function (response) {
-        return response
-    },
-    function (error) {
-        return Promise.reject(error)
-    }
+  function (response) {
+    return response
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
 )
-
-export const socket = io("http://localhost:8081");
-
